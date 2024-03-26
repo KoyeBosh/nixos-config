73a8fb5fb14bfa9ab3305c0ef7f6bd0dfe13a21a
Koye <thekiingofmuffins@gmail.com>
2024-03-26 14:12:22 -0700

    Use zram swap

--- configuration.nix
+++ configuration.nix
@@ -5,244 +5,254 @@
 { config, pkgs, inputs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      inputs.home-manager.nixosModules.default
-    ];
+	imports =
+		[ # Include the results of the hardware scan.
+		./hardware-configuration.nix
+			inputs.home-manager.nixosModules.default
+		];
 
-  # Bootloader.
-  boot.kernelPackages = pkgs.linuxPackages_latest; # somehow not the default on unstable
-  boot.loader.grub.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.grub = {
-    efiSupport = true;
-    useOSProber = true;
-    device = "nodev";
-  };
+# Bootloader.
+	boot.kernelPackages = pkgs.linuxPackages_latest; # somehow not the default on unstable
+		boot.loader.grub.enable = true;
+	boot.loader.efi.canTouchEfiVariables = true;
+	boot.loader.grub = {
+		efiSupport = true;
+		useOSProber = true;
+		device = "nodev";
+	};
 
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+	nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+	networking.hostName = "nixos"; # Define your hostname.
+# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+# Configure network proxy if necessary
+# networking.proxy.default = "http://user:password@proxy:port/";
+# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+# Enable networking
+		networking.networkmanager.enable = true;
 
-  # Set your time zone.
-  time.timeZone = "America/Phoenix";
+# Set your time zone.
+	time.timeZone = "America/Phoenix";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+# Select internationalisation properties.
+	i18n.defaultLocale = "en_US.UTF-8";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
-  };
+	i18n.extraLocaleSettings = {
+		LC_ADDRESS = "en_US.UTF-8";
+		LC_IDENTIFICATION = "en_US.UTF-8";
+		LC_MEASUREMENT = "en_US.UTF-8";
+		LC_MONETARY = "en_US.UTF-8";
+		LC_NAME = "en_US.UTF-8";
+		LC_NUMERIC = "en_US.UTF-8";
+		LC_PAPER = "en_US.UTF-8";
+		LC_TELEPHONE = "en_US.UTF-8";
+		LC_TIME = "en_US.UTF-8";
+	};
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+# Enable the X11 windowing system.
+	services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
-  xdg.portal.enable = true;
+# Enable the GNOME Desktop Environment.
+	services.xserver.displayManager.gdm.enable = true;
+# services.xserver.desktopManager.gnome.enable = true;
+	xdg.portal.enable = true;
 
-  # Configure keymap in X11
-  services.xserver = {
-    xkb.layout = "us";
-    xkb.variant = "";
-  };
+# Configure keymap in X11
+	services.xserver = {
+		xkb.layout = "us";
+		xkb.variant = "";
+	};
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+# Enable CUPS to print documents.
+	services.printing.enable = true;
 
-  services.dbus.implementation = "broker";
-  services.udisks2.enable = true;
+	services.power-profiles-daemon.enable = true;
 
-  # Enable sound with pipewire.
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+	services.dbus.implementation = "broker";
+	services.udisks2.enable = true;
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
+# Enable sound with pipewire.
+	sound.enable = true;
+	hardware.pulseaudio.enable = false;
+	security.rtkit.enable = true;
+	services.pipewire = {
+		enable = true;
+		alsa.enable = true;
+		alsa.support32Bit = true;
+		pulse.enable = true;
+# If you want to use JACK applications, uncomment this
+#jack.enable = true;
 
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = false;
-    settings.General.Experimental = true;
-  };
+# use the example session manager (no others are packaged yet so this is enabled by default,
+# no need to redefine it in your config for now)
+#media-session.enable = true;
+	};
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+	hardware.bluetooth = {
+		enable = true;
+		powerOnBoot = false;
+		settings.General.Experimental = true;
+	};
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users = {
-    users.koye = {
-      isNormalUser = true;
-      description = "Koye";
-      extraGroups = [ "networkmanager" "wheel" ];
-      shell = pkgs.zsh;
-      packages = with pkgs; [
-        discord
-      ];
-    };
-	defaultUserShell = pkgs.zsh; # sets the root user's shell to zsh
-  };
+# Enable touchpad support (enabled default in most desktopManager).
+# services.xserver.libinput.enable = true;
 
-  home-manager = {
-    extraSpecialArgs = { inherit inputs; };
-    users = {
-      "koye" = import ./home.nix;
-    };
-  };
-
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
-  services.xserver.videoDrivers = [ "modesetting" "intel" ];
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-	intel-compute-runtime
-	intel-media-driver
-	gnome.nautilus
-	gradience
-	btop
-	neofetch
-	firefox
-	chromium
-	git
-	gh
-	neovide
-	foot
-	waybar
-	dunst
-	wofi
-	brightnessctl
-	swww
-	fzf
-	hyprpicker
-	slurp
-	sway-contrib.grimshot
-	wl-clipboard
-	swappy
-	easyeffects
-	pavucontrol
-	qogir-icon-theme
-	gcc
-	eza
-	zoxide
-	udiskie
-	ntfs3g
-	swaylock-effects
-	swayidle
-	killall
-	bat
-	bat-extras.batman
-	texliveFull
-	texlab
-	zathura
-  ];
-
-  environment.sessionVariables = {
-    # SDL_VIDEODRIVER = "x11"; # tf2
-  };
-
-  fonts.packages = with pkgs; [
-	twitter-color-emoji
-	noto-fonts
-	noto-fonts-cjk
-	liberation_ttf
-	jetbrains-mono
-	(nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
-  ];
-
-  qt.style = "adwaita";
-  qt.platformTheme = "gnome";
-
-  security.pam.services.swaylock = {};
-
-  programs = {
-    zsh = {
-      enable = true;
-	  shellAliases = {
-		  ls = "eza --color=auto";
-		  grep = "grep --color=auto";
-		  ip = "ip -color=auto";
-		  ll = "ls -l";
-		  la = "ll -a";
-		  c = "clear";
-		  man = "batman";
-	  };
-      syntaxHighlighting.enable = true;
-      enableBashCompletion = true;
-    };
-    neovim = {
-      enable = true;
-	  defaultEditor = true;
-	  vimAlias = true;
-	  viAlias = true;
-    };
-    steam = {
-      enable = true;
-      package = pkgs.steam.override {
-		extraEnv = {
-          SDL_VIDEODRIVER = "x11";
+# Define a user account. Don't forget to set a password with ‘passwd’.
+	users = {
+		users.koye = {
+			isNormalUser = true;
+			description = "Koye";
+			extraGroups = [ "networkmanager" "wheel" ];
+			shell = pkgs.zsh;
+			packages = with pkgs; [
+				discord
+			];
 		};
-      };
-    };
-    hyprland = {
-      enable = true;
-    };
-  };
+		defaultUserShell = pkgs.zsh; # sets the root user's shell to zsh
+	};
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+	home-manager = {
+		extraSpecialArgs = { inherit inputs; };
+		users = {
+			"koye" = import ./home.nix;
+		};
+	};
 
-  # List services that you want to enable:
+# Allow unfree packages
+	nixpkgs.config.allowUnfree = true;
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+	services.xserver.videoDrivers = [ "modesetting" "intel" ];
+# List packages installed in system profile. To search, run:
+# $ nix search wget
+	environment.systemPackages = with pkgs; [
+		jdk8
+		jdt-language-server
+		lua-language-server
+		rust-analyzer
+		intel-compute-runtime
+		intel-media-driver
+		gnome.nautilus
+		gradience
+		btop
+		neofetch
+		firefox
+		chromium
+		git
+		gh
+		neovide
+		foot
+		waybar
+		dunst
+		wofi
+		brightnessctl
+		swww
+		fzf
+		hyprpicker
+		slurp
+		sway-contrib.grimshot
+		wl-clipboard
+		swappy
+		easyeffects
+		pavucontrol
+		qogir-icon-theme
+		gcc
+		eza
+		zoxide
+		udiskie
+		ntfs3g
+		swaylock-effects
+		swayidle
+		killall
+		bat
+		bat-extras.batman
+		texliveFull
+		texlab
+		zathura
+	];
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+	environment.sessionVariables = {
+# SDL_VIDEODRIVER = "x11"; # tf2
+	};
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "23.11"; # Did you read the comment?
+	fonts.packages = with pkgs; [
+		twitter-color-emoji
+		noto-fonts
+		noto-fonts-cjk
+		liberation_ttf
+		jetbrains-mono
+		(nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+	];
+
+	qt.style = "adwaita";
+	qt.platformTheme = "gnome";
+
+	security.pam.services.swaylock = {};
+
+	programs = {
+		zsh = {
+			enable = true;
+			shellAliases = {
+				ls = "eza --color=auto";
+				grep = "grep --color=auto";
+				ip = "ip -color=auto";
+				ll = "ls -l";
+				la = "ll -a";
+				c = "clear";
+				man = "batman";
+			};
+			syntaxHighlighting.enable = true;
+			enableBashCompletion = true;
+		};
+		neovim = {
+			enable = true;
+			defaultEditor = true;
+			vimAlias = true;
+			viAlias = true;
+		};
+		steam = {
+			enable = true;
+			package = pkgs.steam.override {
+				extraEnv = {
+					SDL_VIDEODRIVER = "x11";
+				};
+			};
+		};
+		hyprland = {
+			enable = true;
+		};
+	};
+
+	zramSwap = {
+		enable = true;
+		memoryPercent = 100;
+	};
+# Some programs need SUID wrappers, can be configured further or are
+# started in user sessions.
+# programs.mtr.enable = true;
+# programs.gnupg.agent = {
+#   enable = true;
+#   enableSSHSupport = true;
+# };
+
+# List services that you want to enable:
+
+# Enable the OpenSSH daemon.
+# services.openssh.enable = true;
+
+# Open ports in the firewall.
+# networking.firewall.allowedTCPPorts = [ ... ];
+# networking.firewall.allowedUDPPorts = [ ... ];
+# Or disable the firewall altogether.
+# networking.firewall.enable = false;
+
+# This value determines the NixOS release from which the default
+# settings for stateful data, like file locations and database versions
+# on your system were taken. It‘s perfectly fine and recommended to leave
+# this value at the release version of the first install of this system.
+# Before changing this value read the documentation for this option
+# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+	system.stateVersion = "23.11"; # Did you read the comment?
 
 }
